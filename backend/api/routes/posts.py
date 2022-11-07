7219ed5dac2ef4f6518519b523cb5864785b9a18
khaledha <khaledhadjali1@gmail.com>
2022-11-07 17:42:55 +0100

    adding post routes update

--- backend/api/routes/posts.py
+++ backend/api/routes/posts.py
@@ -2,11 +2,12 @@ from datetime import datetime
 from fastapi import Depends, HTTPException, status, APIRouter, Response, Body
 from pymongo.collection import ReturnDocument
 from config.mongo_config import Post
-from methodes.posts import add_post, retrieve_post, retrieve_posts
+from methodes.posts import add_post, retrieve_post, retrieve_posts, update_post
 from validators.post_validators import (
     CreatePostSchema,
     ErrorResponseModel,
     ResponseModel,
+    UpdatePostSchema,
 )
 from fastapi.encoders import jsonable_encoder
 from bson.objectid import ObjectId
@@ -35,3 +36,19 @@ def get_post_data(id):
     if post:
         return ResponseModel(post, 'Post data retrieved successfully')
     return ErrorResponseModel('An error occurred.', 404, "Post doesn't exist.")
+
+
+@post_router.put('/{id}')
+async def update_post_data(id: str, req: UpdatePostSchema = Body(...)):
+    req = {k: v for k, v in req.dict().items() if v is not None}
+    updated_post = update_post(id, req)
+    if updated_post:
+        return ResponseModel(
+            'post with ID: {} name update is successful'.format(id),
+            'post updated successfully',
+        )
+    return ErrorResponseModel(
+        'An error occurred',
+        404,
+        'There was an error updating the post data.',
+    )
